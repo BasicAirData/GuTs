185fddd7fe9c1ed4285a4ec41a34f1752dfb4bf3
JLJu <info@basicairdata.eu>
2025-02-04 20:45:22 +0100

    Updated license

--- Scripts/GUI/inter3.py
+++ Scripts/GUI/inter3.py
@@ -1,3 +1,24 @@
+'''
+* Created by J.Larragueta on 04/02/2025
+ * This file is part of BasicAirData GPS Logger Utils
+ *
+ * Copyright (C) 2011 BasicAirData
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+'''
+
 # Import the required libraries
 from tkinter import *
 from tkinter import ttk
